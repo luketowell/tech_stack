b1f367e7773f87192a49ec18be6e0f2316cefa3c
Unknown <luke.towell@hotmail.com>
2018-07-09 19:14:15 +0100

    more changes to resolve conflicts

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 import { AppRegistry } from 'react-native';
-<<<<<<< Updated upstream
-import App from './App';
-=======
 import App from './src/App';
->>>>>>> Stashed changes
 
 AppRegistry.registerComponent('tech_stack', () => App);
